39b0301df8e5318f0e2aede0d1576116f2ba17f3
magalutfullaev <mhoja9494@gmail.com>
2023-10-12 00:01:47 +0500

    cleaning project

---
+++ src/views/MainView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView'
+})
+</script>
+
+<style lang="scss">
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+  },
 })
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/_variables.scss'),
+      ],
+    })
+}
