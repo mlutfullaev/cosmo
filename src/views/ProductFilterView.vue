bee9ba5461734a29cf4f33bfdd7d7760c62b5329
magalutfullaev <mhoja9494@gmail.com>
2023-11-02 15:56:09 +0500

    add: ProductFilter

--- src/views/ProductFilterView.vue
+++ src/views/ProductFilterView.vue
@@ -2,7 +2,47 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  components: {}
+  components: {},
+  data: () => ({
+    activeTab: 0,
+    filters: [
+      {
+        title: 'Age targeted group',
+        subtitle: 'Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/',
+        contentTitle: 'Please select Your age group to make relevant search',
+        variants: ['18 - 25', '25 - 35', '35 - 55', '0-6', '6 - 14', '14 - 18', '55 +'],
+        selectedVariant: '0-6',
+      },
+      {
+        title: 'Targeted Concerns',
+        subtitle: 'Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/',
+        contentTitle: 'Please select Your age group to make relevant search',
+        variants: ['18 - 25', '25 - 35', '35 - 55', '0-6', '6 - 14', '14 - 18', '55 +'],
+        selectedVariant: '0-6',
+      },
+      {
+        title: 'Product Claims (Vegan, Natural, etc)',
+        subtitle: 'Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/',
+        contentTitle: 'Please select Your age group to make relevant search',
+        variants: ['18 - 25', '25 - 35', '35 - 55', '0-6', '6 - 14', '14 - 18', '55 +'],
+        selectedVariant: '0-6',
+      },
+      {
+        title: 'Religious Certified Products',
+        subtitle: 'Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/',
+        contentTitle: 'Please select Your age group to make relevant search',
+        variants: ['18 - 25', '25 - 35', '35 - 55', '0-6', '6 - 14', '14 - 18', '55 +'],
+        selectedVariant: '0-6',
+      },
+      {
+        title: 'Targeted Skin Types',
+        subtitle: 'Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/',
+        contentTitle: 'Please select Your age group to make relevant search',
+        variants: ['18 - 25', '25 - 35', '35 - 55', '0-6', '6 - 14', '14 - 18', '55 +'],
+        selectedVariant: '0-6',
+      },
+    ]
+  })
 })
 </script>
 
@@ -19,19 +59,52 @@ export default defineComponent({
       <p class="txt">Please select the targeted age group. If the product does not have age specification or warnings for age </p>
     </div>
     <div class="productFilter-inner">
-      <div class="productFilter-tabs">
-        <div class="productFilter-tab">
-          <button>
-            Age targeted group
-          </button>
-          <p class="txt">
-            Please select the targeted age group. If the product does not have age specification or warnings for age use, we will show it presnt it in any selection/
-          </p>
+      <div class="filter-tabs">
+        <div
+          v-for="(filterTab, idx) in filters"
+          class="filter-tab"
+          @click="activeTab = idx"
+          :class="{active: activeTab === idx}"
+          :key="filterTab.title">
+            <h3>{{filterTab.title}}</h3>
+            <p>{{filterTab.subtitle}}</p>
+            <div class="content-tab active">
+              <p class="txt-highlight">{{filterTab.contentTitle}}</p>
+              <div class="filter-buttons">
+                <button
+                  v-for="variant in filterTab.variants"
+                  :class="{active: variant === filterTab.selectedVariant}"
+                  @click="filterTab.selectedVariant = variant"
+                  class="filter-button"
+                  :key="variant">
+                  {{variant}}
+                </button>
+              </div>
+            </div>
         </div>
       </div>
       <div class="content">
-
+        <div
+          class="content-tab"
+          v-for="(filterTab, idx) in filters"
+          :class="{active: activeTab === idx}"
+          :key="filterTab.title">
+          <p class="txt-highlight">{{filterTab.contentTitle}}</p>
+          <div class="filter-buttons">
+            <button
+              v-for="variant in filterTab.variants"
+              :class="{active: variant === filterTab.selectedVariant}"
+              @click="filterTab.selectedVariant = variant"
+              class="filter-button"
+              :key="variant">
+              {{variant}}
+            </button>
+          </div>
+        </div>
       </div>
+    </div>
+    <div class="productFilter-bottom">
+      <RouterLink to="/" class="link bold">search <span>→</span></RouterLink>
     </div>
     <TheFooter />
   </div>
@@ -46,13 +119,71 @@ export default defineComponent({
     display: grid;
     align-items: center;
     justify-content: center;
+    text-align: center;
 
     a {
       position: absolute;
       top: 50%;
       transform: translateY(-50%);
       left: 30px;
+      @media (max-width: 1200px) {
+        display: none;
+      }
     }
+    p {
+      max-width: 400px;
+      margin: 0 auto;
+    }
+    @media (max-width: 768px) {
+      padding: 30px;
+    }
+    @media (max-width: 480px) {
+      .title {
+        font-size: 24px;
+      }
+    }
+  }
+  &-inner {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border-top: 1px solid $black;
+
+    .content-tab {
+      border-left: 1px solid $black;
+      border-bottom: 1px solid $black;
+      display: none;
+      padding: 40px;
+      height: 100%;
+
+      &.active {
+        display: block;
+      }
+      p {
+        padding-bottom: 20px;
+      }
+      @media (max-width: 1000px) {
+        padding: 40px 20px;
+      }
+    }
+    .filter-buttons {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 20px;
+      height: max-content;
+      align-items: center;
+    }
+
+    @media (max-width: 600px) {
+      grid-template-columns: 1fr;
+
+      .content {
+        display: none;
+      }
+    }
+  }
+  &-bottom {
+    padding: 60px;
+    text-align: center;
   }
 }
 </style>
